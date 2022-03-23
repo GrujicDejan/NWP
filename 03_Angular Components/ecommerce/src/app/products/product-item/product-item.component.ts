00c115d726329285d31c83b79870979193ea1969
GrujicDejan <dejan.grujic2000@gmail.com>
2022-03-23 20:16:57 +0100

    update component

--- 03_Angular Components/ecommerce/src/app/products/product-item/product-item.component.ts
+++ 03_Angular Components/ecommerce/src/app/products/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-product-item',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductItemComponent implements OnInit {
 
-  constructor() { }
+  public product!: Product;
 
-  ngOnInit(): void {
+  constructor() {
+    this.product = {
+      name: "Dog food",
+      imageURL: "https://pngimg.com/uploads/dog_food/dog_food_PNG53.png",
+      price: 300,
+      isOnSale: true,
+      quantity: 0
+    }
   }
 
+  ngOnInit(): void {
+    
+  }
+
+  increaseQuantity() {
+    this.product.quantity++;
+  }
+
+  decreaseQuantity() {
+    if (this.product.quantity > 0) {
+      this.product.quantity--;
+    }
+  }
 }
