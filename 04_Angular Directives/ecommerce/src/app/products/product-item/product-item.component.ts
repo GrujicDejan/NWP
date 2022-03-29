8116effa8112b7c97c07a6d2d5ba18fe5f9c0340
GrujicDejan <dejan.grujic2000@gmail.com>
2022-03-29 23:32:20 +0200

    finished task 1

--- 04_Angular Directives/ecommerce/src/app/products/product-item/product-item.component.ts
+++ 04_Angular Directives/ecommerce/src/app/products/product-item/product-item.component.ts
@@ -9,12 +9,28 @@ import { Product } from '../model/product';
 export class ProductItemComponent implements OnInit {
 
   public product!: Product;
+  public product2!: Product;
+  public product3!: Product;
 
   constructor() {
     this.product = {
       name: "Dog food",
       imageURL: "https://pngimg.com/uploads/dog_food/dog_food_PNG53.png",
       price: 300,
+      isOnSale: true,
+      quantity: 0
+    }
+    this.product2 = {
+      name: "Dog food 2",
+      imageURL: "https://pngimg.com/uploads/dog_food/dog_food_PNG53.png",
+      price: 500,
+      isOnSale: false,
+      quantity: 0
+    }
+    this.product3 = {
+      name: "Dog food 3",
+      imageURL: "https://pngimg.com/uploads/dog_food/dog_food_PNG53.png",
+      price: 400,
       isOnSale: true,
       quantity: 0
     }
