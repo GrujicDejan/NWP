1a7f6e4ece1927d1c0d442e7d1117029ec27c748
GrujicDejan <dejan.grujic2000@gmail.com>
2022-04-22 15:51:28 +0200

    finished task 5

--- 05_Form groups/ecommerce/src/app/products/create-product/create-product.component.ts
+++ 05_Form groups/ecommerce/src/app/products/create-product/create-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../model/product';
 
 @Component({
   selector: 'app-create-product',
@@ -12,6 +13,17 @@ export class CreateProductComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  createProduct(productForm:any) {
+    if (productForm.invalid) {
+      this.message = "Please correct all errors and try again"
+    } else {
+      let product : Product = productForm.value.product;
+      console.log(productForm)
+      console.log(productForm.value)
+      console.log(product)
+    }
   }
 
 }
