a74a5acf4728f04d9fbbf31018c18a9fd31e8600
GrujicDejan <dejan.grujic2000@gmail.com>
2022-06-06 12:02:11 +0200

    added sort for list

--- 11_Exercise/MovieInfo/src/app/services/movie.service.ts
+++ 11_Exercise/MovieInfo/src/app/services/movie.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MovieInfoModel } from '../model/movieInfoModel';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class MovieService {
@@ -11,9 +12,12 @@ export class MovieService {
   BACKEND_BASE = "http://localhost:3000";
 
   getMovies(query: string): Observable<any> {
+    
     return this.httpClient.get<MovieInfoModel[]>(this.BACKEND_BASE + "/api/movie", {
       params: {query: query}
-    })
+    }).pipe(
+      map(res=>res.sort((x,y) => x.year < y.year? 1 : -1))
+    )
   }
 
   changeRating(name: string, changeAmount: number): Observable<any> {
